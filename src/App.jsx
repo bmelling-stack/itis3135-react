ba6544bc771f664ac02c19b02e365a0b7288172a
Brian <bjmellin@charlotte.edu>
2025-11-10 18:33:14 -0500

    attempt to fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import './App.css'
+import Header from './Header'
+import Footer from './Footer'
 import Index from './Index'
 import Introduction from './Introduction'
 import Contract from './Contract'
-import Header from './Header'
-import Footer from './Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <BrowserRouter>
+      <Routes>
+       <Route path='/' element={<Index/>}>
+        <Route path='/introduction' element={<Introduction/>}></Route>
+        <Route path='/contract' element={<Contract/>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './default.css'
+import './index.css'
 import App from './App.jsx'
-import Introduction from './Introduction.jsx'
-import Index from './Index.jsx'
-import Contract from './Contract.jsx'
-import Layout from './Layout.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-       <Route path='/' element={<Layout/>}>
-        <Route path='/' element={<App/>}></Route>
-        <Route path='/index.jsx' element={<Index/>}></Route>
-        <Route path='/introduction' element={<Introduction/>}></Route>
-        <Route path='/contract' element={<Contract/>}></Route>
-        </Route>
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
