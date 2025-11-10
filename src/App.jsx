30703947da7711544fe3ef467517a756bc4c65c3
Brian <bjmellin@charlotte.edu>
2025-11-10 00:24:31 -0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <a href="../src/Home.jsx">Home</a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,9 +3,9 @@ export default function Header() {
         <header>
             <h1>Brian Mellinger ~ Benevolent Moose</h1>
             <nav>
-                <a href="../src/Index.jsx">Home</a> ~ 
-                <a href="itis3135-react\src\Introduction.jsx">Introduction</a> ~
-                <a href="C:\Users\Brian\Downloads\ITIS3135\bmelling-react\itis3135-react\src\Contract.jsx">Contract</a>
+                <a href="../Home.jsx">Home</a> ~ 
+                <a href="itis3135-react/src/Introduction.jsx">Introduction</a> ~
+                <a href="itis3135-react/src/Contract.jsx">Contract</a>
             </nav>
         </header>
     );
