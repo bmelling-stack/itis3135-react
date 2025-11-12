ff69c18b19d6e3215f64ba43893c4c8410c5bfa4
Brian <bjmellin@charlotte.edu>
2025-11-12 15:03:34 -0500

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
-import { Outlet } from 'react-router';
+import { Outlet } from 'react-router-dom';
 
 export default function Layout () {
     return (
