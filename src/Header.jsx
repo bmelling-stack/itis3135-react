ffb416db246e6e4753f1b938f3ae9387eb785c46
Brian <bjmellin@charlotte.edu>
2025-11-12 15:01:00 -0500

    changed href to lowecase

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
             <h1>Brian Mellinger ~ Benevolent Moose</h1>
             <nav>
                 <a href="/">Home</a> ~ 
-                <a href="/Introduction">Introduction</a> ~
+                <a href="/introduction">Introduction</a> ~
                 <a href="/contract">Contract</a>
             </nav>
         </header>
